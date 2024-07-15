8cba2bc483e952e061a245c0d628890c93d05fd8
Alexander Scherbakov <webmasterdracarys@gmail.com>
2024-07-15 14:38:08 +0300

    created env variables and update docker-compose.yml file

--- time-app/api/src/utils/mysqlPool.mjs
+++ time-app/api/src/utils/mysqlPool.mjs
@@ -1,12 +1,20 @@
 import mysql from 'mysql2'
 
+const MYSQL_HOST = process.env.MY_SQL_HOST || 'mysql'
+const MYSQL_USER = process.env.MY_SQL_USER || 'root'
+const MYSQL_PORT = process.env.MY_SQL_PORT || '3306'
+const MYSQL_PASSWORD = process.env.MY_SQL_PASSWORD || 'password'
+const MYSQL_DB = process.env.MY_SQL_DB || 'admin'
+
+console.log(process.env)
+
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'mysql',
-    port: '3306',
-    user: 'root',
-    password: 'password',
-    database: 'time_db',
+    host: MYSQL_HOST,
+    port: MYSQL_PORT,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DB,
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
